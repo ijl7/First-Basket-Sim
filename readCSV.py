97be646e881245ab359a2b625608dc4002d0573b
ijl7 <62620407+ijl7@users.noreply.github.com>
2023-10-07 20:54:27 -0400

    updated logic to skip missing files

--- readCSV.py
+++ readCSV.py
@@ -1,16 +1,17 @@
 import csv
 #import scraper
 
-i = 22200658
-while i < 22200659:
-    with open('2022\\' + str(i) + '.csv', mode='r') as file:
-        csvFile = csv.DictReader(file)
+i = 22200001
+while i < 22201230:
+    if i != 22200674 and i != 22200714:
+        with open('2022\\' + str(i) + '.csv', mode='r') as file:
+            csvFile = csv.DictReader(file)
 
-        columnNames = []
+            columnNames = []
 
-        for row in csvFile:
-            if "1" == row['eventmsgtype']:
-                print(row['away_team_abbrev'] + " @ " + row['home_team_abbrev'] + ", " + row['player1_name'])
-                break
+            for row in csvFile:
+                if "1" == row['eventmsgtype']:
+                    print(row['away_team_abbrev'] + " @ " + row['home_team_abbrev'] + ", " + row['player1_name'])
+                    break
 
     i += 1
