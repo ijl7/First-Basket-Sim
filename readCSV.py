7dc53343cda9ce622c2dbffe7c44c91ccd5b2f0c
ijl7 <62620407+ijl7@users.noreply.github.com>
2023-10-07 21:18:48 -0400

    got lineups at tip and added last game

--- readCSV.py
+++ readCSV.py
@@ -2,15 +2,28 @@ import csv
 #import scraper
 
 i = 22200001
-while i < 22201230:
+while i < 22201231:
     if i != 22200674 and i != 22200714:
         with open('2022\\' + str(i) + '.csv', mode='r') as file:
             csvFile = csv.DictReader(file)
 
-            columnNames = []
+            homeLineup = []
+            awayLineup = []
 
             for row in csvFile:
                 if "10" == row['eventmsgtype']:
+                    homeLineup.append(row['home_player_1'])
+                    homeLineup.append(row['home_player_2'])
+                    homeLineup.append(row['home_player_3'])
+                    homeLineup.append(row['home_player_4'])
+                    homeLineup.append(row['home_player_5'])
+                    awayLineup.append(row['away_player_1'])
+                    awayLineup.append(row['away_player_2'])
+                    awayLineup.append(row['away_player_3'])
+                    awayLineup.append(row['away_player_4'])
+                    awayLineup.append(row['away_player_5'])
+                    print(homeLineup)
+                    print(awayLineup)
                     print(row['player1_name'] + ' vs ' + row['player2_name'] + ', ' + row['player3_team_abbreviation'] + ' wins tip')
                     break
 
