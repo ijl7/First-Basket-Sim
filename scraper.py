1dde543f406812f1213a6986eacfce6b65a98d49
ijl7 <62620407+ijl7@users.noreply.github.com>
2023-10-26 13:46:14 -0400

    scraped 2023 preseason games

--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
 import nba_scraper.nba_scraper as ns
 
-i = 22100717
-while i < 22100718:
+i = 12300001
+while i < 12300066:
 
-    ns.scrape_game([i], data_format='csv', data_dir="2021")
+    ns.scrape_game([i], data_format='csv', data_dir="preseason")
     #ns.scrape_date_range('2023-10-25','2023-10-25',data_format='csv', data_dir='2023')
     #ns.scrape_season(2024,data_format='csv', data_dir='2023')
     i += 1
