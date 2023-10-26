3c9189bea5cf0176235602d288e4aa6bf19785b9
ijl7 <62620407+ijl7@users.noreply.github.com>
2023-10-26 14:13:22 -0400

    scraped the 2022 playoffs

--- scraper.py
+++ scraper.py
@@ -1,9 +1,12 @@
 import nba_scraper.nba_scraper as ns
 
-i = 12300001
-while i < 12300066:
-
-    ns.scrape_game([i], data_format='csv', data_dir="preseason")
+i = 42200405
+while i < 42200406:
+    try:
+        ns.scrape_game([i], data_format='csv', data_dir="2022 PLAY")
+    except:
+        i += 1
+        continue
     #ns.scrape_date_range('2023-10-25','2023-10-25',data_format='csv', data_dir='2023')
     #ns.scrape_season(2024,data_format='csv', data_dir='2023')
     i += 1
