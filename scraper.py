40247c8b44712f313e1827cb39010e9a44d4ce4a
ijl7 <62620407+ijl7@users.noreply.github.com>
2024-02-06 19:25:19 -0500

    pulled more games

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import nba_scraper.nba_scraper as ns
 
-i = 22300704
-while i < 22300715:
+i = 22300719
+while i < 22300729:
     try:
         ns.scrape_game([i], data_format='csv', data_dir="2023")
     except:
