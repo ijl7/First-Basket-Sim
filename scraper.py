91f5dbc417842062706eb59d3744a17452dc7ce6
ijl7 <62620407+ijl7@users.noreply.github.com>
2024-02-22 17:58:10 -0500

    pulled some missing games

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import nba_scraper.nba_scraper as ns
 
-i = 22300790
-while i < 22300795:
+i = 22300792
+while i < 22300810:
     try:
         ns.scrape_game([i], data_format='csv', data_dir="2023")
     except:
@@ -13,6 +13,6 @@ while i < 22300795:
     i += 1
 
 #Missing Files
-#22100717, 22101171, 22200674, 22200714, 22300425, 22300576, 22300589
+#22100717, 22101171, 22200674, 22200714, 22300425, 22300589
 #Broken Files
 #22100773
